27258458732995eed9e0a2645242e2a07d8197dd
Gustavo Santos <santosgustavo431@gmail.com>
2021-05-12 17:46:42 -0300

    Ajuste colunas

--- src/main/java/com/gsantos/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/gsantos/minhasfinancas/model/entity/Usuario.java
@@ -10,14 +10,11 @@ import java.util.Objects;
 public class Usuario {
 
     @Id
-    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome")
     private String nome;
 
-    @Column(name = "email")
     private String email;
 
     @Override
