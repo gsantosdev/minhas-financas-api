2fa3a558b12852859c2dc00134ad9ab2e053e8e6
Gustavo Santos <santosgustavo431@gmail.com>
2021-05-15 18:43:26 -0300

    Teste Email

--- src/test/java/com/gsantos/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/gsantos/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,6 +1,11 @@
 package com.gsantos.minhasfinancas.service;
 
 
+import com.gsantos.minhasfinancas.exception.RegraNegocioException;
+import com.gsantos.minhasfinancas.model.entity.Usuario;
+import com.gsantos.minhasfinancas.model.repository.UsuarioRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,5 +19,34 @@ public class UsuarioServiceTest {
 
     @Autowired
     UsuarioService service;
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Test
+    public void deveValidarEmail(){
+        Assertions.assertDoesNotThrow(() ->{
+
+            //cenario
+            repository.deleteAll();
+
+            //acao
+            service.validarEmail("email@gmail.com");
+        });
+    }
+
+    @Test
+    public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado(){
+        Assertions.assertThrows(RegraNegocioException.class, ()->
+        {
+            //cenario
+            Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
+
+            //acao
+            service.validarEmail("email@email.com");
+        });
+
+    }
+
 
 }
