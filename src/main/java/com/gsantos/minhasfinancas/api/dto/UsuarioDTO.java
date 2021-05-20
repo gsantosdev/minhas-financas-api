d0b93613bbe757df8c6d8be9eae2447efe77442f
Gustavo Santos <santosgustavo431@gmail.com>
2021-05-20 17:04:28 -0300

    LancamentoService

--- src/main/java/com/gsantos/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/gsantos/minhasfinancas/api/dto/UsuarioDTO.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Builder
 public class UsuarioDTO {
     private String email;
-    private String senha;
     private String nome;
+    private String senha;
 
 }
---
+++ src/main/java/com/gsantos/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,20 @@
+package com.gsantos.minhasfinancas.service;
+
+import com.gsantos.minhasfinancas.model.entity.Lancamento;
+import com.gsantos.minhasfinancas.model.enums.StatusLancamento;
+
+import java.util.List;
+
+public interface LancamentoService {
+
+    Lancamento salvar(Lancamento lancamento);
+
+    Lancamento atualizar(Lancamento lancamento);
+
+    void deletar(Lancamento lancamento);
+
+    List<Lancamento> buscar(Lancamento lancamentoFiltro);
+
+    void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento);
+
+}
