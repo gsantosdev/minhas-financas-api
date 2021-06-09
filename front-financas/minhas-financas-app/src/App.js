4cbb33e4c74dfda82318638d0c9f753762c5f260
gsantosdev <santosgustavo431@gmail.com>
2021-06-09 17:17:34 -0300

    Utilizando bootswatch

--- front-financas/minhas-financas-app/src/App.js
+++ front-financas/minhas-financas-app/src/App.js
@@ -1,33 +1,17 @@
 import React from 'react';
 
+import 'bootswatch/dist/flatly/bootstrap.css'
+
+import Login from './views/login'
+
 class App extends React.Component {
 
-  state = {
-    numero1: '',
-    numero2: '',
-    resultado: ''
-  }
-
-  somar = () =>{
-    const resultado =  parseInt(this.state.numero1) + parseInt(this.state.numero2)
-    this.setState({ resultado: resultado})
-
-  }
 
   render() {
     return (
-      <div>
-        <label>Número 1:</label>
-        <input type="text" value={this.state.nome} onChange={(e) => this.setState({ numero1: e.target.value })} />
-        <br />
-        <label>Número 2:</label>
-        <input type="text" value={this.state.nome} onChange={(e) => this.setState({ numero2: e.target.value })} />
-        <br />
-        <button onClick={this.somar}>Somar</button>
-        <br />
-        A soma de numero1 com numero2: {this.state.resultado}
-      </div>
+      <Login />
     )
+
   }
 }
 
